876fb9849f80896bcb387ef28f0bd5172df60c55
sergi <sergiferregonzalez@gmail.com>
2025-11-16 19:13:28 +0100

    Fix: Codificacion de archivos a UTF8 limpia con Powershell

--- api/logger.js
+++ api/logger.js
@@ -1,8 +1,8 @@
-// api/logger.js
+﻿// api/logger.js
 export default async (req, res) => {
-    // La función debe usar la sintaxis 'export default'
+    // La funciÃ³n debe usar la sintaxis 'export default'
 
-    // 1. Obtener la información del User-Agent
+    // 1. Obtener la informaciÃ³n del User-Agent
     const userAgent = req.headers['user-agent'] || 'Desconocido';
 
     // 2. Registrar la visita en los logs de Vercel
@@ -12,7 +12,7 @@ export default async (req, res) => {
     // 3. Devolver una respuesta JSON al cliente (usando res.status(200).json)
     res.status(200).json({
         status: "success",
-        message: "Petición recibida éticamente.",
+        message: "PeticiÃ³n recibida Ã©ticamente.",
         user_agent: userAgent
     });
 };
